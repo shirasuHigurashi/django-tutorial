bf36a702940728ce8e6d1f111c4f4716f237dfff
shirasuHigurashi <dnakazaw@gmail.com>
2025-04-23 04:45:53 +0900

    second

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,29 @@
+from django.shortcuts import render,redirect
+from http.client import HTTPResponse
+from .models import Post
 from django.shortcuts import render
-
+from .forms import CommentForm
 # Create your views here.
 def frontpage(request):
-    return render(request, "blog/frontpage.html")
+    posts = Post.objects.all()
+    return render(request, "blog/frontpage.html",{"posts":posts})
+
+
+
+def post_detail(request,slug):
+    post = Post.objects.get(slug=slug)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+                comment = form.save(commit=False)
+                comment.post = post
+                comment.save()
+                return redirect("post_detail",slug=post.slug)
+    else:
+        form = CommentForm()
+    return render(request,"blog/post_detail.html",{"post":post,"form":form})
+
+
+
+    
